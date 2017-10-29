3174c8931293df4fceadab2a7dc44708c01b23e9
Aleksandr Shevkunenko <[Aleksandr_Shevkunenko@epam.com]>
2017-10-29 22:15:34 +0300

    Lab 8. Country refactored.

--- tx/src/main/java/model/Country.java
+++ tx/src/main/java/model/Country.java
@@ -2,10 +2,12 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Country implements Serializable {
 
